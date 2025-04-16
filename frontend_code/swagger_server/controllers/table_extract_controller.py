3154c853440ade36ac3b5837c215dc660b2d4f8e
jiongjiongli <33146359+jiongjiongli@users.noreply.github.com>
2025-04-16 00:48:03 -0500

    Update.

--- frontend_code/swagger_server/controllers/table_extract_controller.py
+++ frontend_code/swagger_server/controllers/table_extract_controller.py
@@ -12,14 +12,17 @@ from model_service import get_args, TableExtractModel
 def create_model():
     dir_name_postfix = "service"
     input_args = [
+        '--use_ocr', True,
         '--mode', 'extract',
-        '--image_dir', f'/home/cc/team20/data/tiny_fintabnet_database/page_images/{dir_name_postfix}',
-        '--words_dir', f'/home/cc/team20/data/tiny_fintabnet_database/ocr_results/{dir_name_postfix}',
-        '--out_dir', f'/home/cc/team20/data/inference_output/{dir_name_postfix}',
-        '--structure_config_path', '/home/cc/team20/proj/git/table-transformer/src/structure_config.json',
-        '--structure_model_path', '/home/cc/team20/models/TATR-v1.1-Fin-msft.pth',
-        '--detection_config_path', '/home/cc/team20/proj/git/table-transformer/src/detection_config.json',
-        '--detection_model_path', '/home/cc/team20/models/pubtables1m_detection_detr_r18.pth',
+        '--image_dir',             f'/home/cc/team20/data/tiny_fintabnet_database/page_images/{dir_name_postfix}',
+        '--page_words_dir',        f'/home/cc/team20/data/tiny_fintabnet_database/FinTabNet.c_PDF_Annotations_JSON',
+        '--table_words_dir',       f'/home/cc/team20/data/tiny_fintabnet_database/FinTabNet.c_Image_Table_Words_JSON',
+        '--ocr_words_dir',         f'/home/cc/team20/data/tiny_fintabnet_database/ocr_results/{dir_name_postfix}',
+        '--out_dir',               f'/home/cc/team20/data/tiny_fintabnet_database/inference_output/{dir_name_postfix}',
+        '--structure_config_path', f'/home/cc/team20/proj/git/table-transformer/src/structure_config.json',
+        '--structure_model_path',  f'/home/cc/team20/models/TATR-v1.1-Fin-msft.pth',
+        '--detection_config_path', f'/home/cc/team20/proj/git/table-transformer/src/detection_config.json',
+        '--detection_model_path',  f'/home/cc/team20/models/pubtables1m_detection_detr_r18.pth',
         '--html',
         '--csv',
         '--objects',
@@ -55,6 +58,9 @@ def table_extract_post(file):
 
     mode = query_params["mode"]
     args.mode = mode.lower()
+
+    ocr_option = query_params["OCR"]
+    args.use_ocr = ocr_option.lower() == "Tesseract OCR"
 
     print(f"processing: {image_file_path}")
     output_file_path = model.process(str(image_file_path))
